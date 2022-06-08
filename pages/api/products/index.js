24d1050e7fd466233fb4b4cdd90cc8bfaa65d5e0
Myr7You <myr7you@gmail.com>
2022-06-08 22:46:22 +0800

    change env

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     try {
       const products = await Product.find();
       res.status(200).json(products);
-      console.log(products);
+      // console.log(products);
     } catch (error) {
       res.status(500).json(error)
     }
