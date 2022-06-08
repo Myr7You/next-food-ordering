2f8e1eaf5a530cfa6d2fd854763d2022a62405a6
Myr7You <myr7you@gmail.com>
2022-06-08 21:16:40 +0800

    change config

--- next.config.js
+++ next.config.js
@@ -2,13 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_HOST: 'http://localhost:3000',
+    // NEXT_PUBLIC_HOST: 'http://localhost:3000',
+    NEXT_PUBLIC_HOST: 'https://next-food-ordering-phi.vercel.app/',
     NEXT_PUBLIC_USERNAME: 'admin',
     NEXT_PUBLIC_PASSWORD: 123456,
     NEXT_PUBLIC_TOKEN: 'SWdw4Cv||663Z{p3ZXtp%k42sfjj4f'
   },
-  images:{
-    domains:["res.cloudinary.com"]
+  images: {
+    domains: ['res.cloudinary.com']
   }
 };
 
